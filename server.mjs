8c9ce0ce878946ce81f503f1e987a4df0ada1c38
unknown <subhanabidi211@gmail>
2023-01-05 09:25:39 +0500

    commit message

---
+++ server.mjs
@@ -0,0 +1,42 @@
+import express from 'express';
+import path from 'path';
+
+const app = express()
+const port = process.env.PORT || 5001;
+
+
+
+app.get('/abc', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
+
+app.get('/weather', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send({
+        temp: 30,
+        humidity: 72,
+        serverTime: new Date().toString()
+    });
+})
+
+
+
+app.get('/time', (req, res) => {
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
+})
+
+
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
